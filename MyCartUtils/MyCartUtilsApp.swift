1c6edb00bde3c708f07362f80e5873333221eab2
mohammed.ibrahim <mohammed.ibrahim@kydonlsg.com>
2021-07-01 21:42:03 +0530

    Initial Commit

---
+++ MyCartUtils/MyCartUtilsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCartUtilsApp.swift
+//  MyCartUtils
+//
+//  Created by Mohammed Ibrahim on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MyCartUtilsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
